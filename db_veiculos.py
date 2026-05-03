f2dc21b55a91050d6693a72a2faa2a954638cd18
RusselShark1 <marco.guellis@gmail.com>
2026-05-03 14:43:32 -0300

    ajuste na linha 11 a 14 , values

--- db_veiculos.py
+++ db_veiculos.py
@@ -6,11 +6,11 @@ banco = sqlite3.connect('db_carros')
 
 cursor = banco.cursor()
 
-cursor.execute("CREATE TABLE veiculos(modelo text,placa text, km_final integer)")
+#cursor.execute("CREATE TABLE veiculos(modelo text,placa text, km_final integer)")
 
-cursor.execute("INSERT INTO veiculos ('Fiorino', 'CIY4H17', 35000)")
-cursor.execute("INSERT INTO veiculos ('Fiorino', 'CIY5H18', 45000)")
-cursor.execute("INSERT INTO veiculos ('Fiorino', 'BDY4H17', 70000)")
-cursor.execute("INSERT INTO veiculos ('Fiorino', 'LDP4H24', 10000)")
+cursor.execute("INSERT INTO veiculos VALUES('Fiorino', 'CIY4H17', 35000)")
+cursor.execute("INSERT INTO veiculos VALUES('Fiorino', 'CIY5H18', 45000)")
+cursor.execute("INSERT INTO veiculos VALUES('Fiorino', 'BDY4H17', 70000)")
+cursor.execute("INSERT INTO veiculos VALUES('Fiorino', 'LDP4H24', 10000)")
 
 banco.commit
