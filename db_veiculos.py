1f28d1d21a0ca168749dce94e2ee97e7577f4795
RusselShark1 <marco.guellis@gmail.com>
2026-05-03 15:02:59 -0300

    Atulização do bando de veiculos

--- db_veiculos.py
+++ db_veiculos.py
@@ -6,11 +6,14 @@ banco = sqlite3.connect('db_carros')
 
 cursor = banco.cursor()
 
-#cursor.execute("CREATE TABLE veiculos(modelo text,placa text, km_final integer)")
+""" cursor.execute("CREATE TABLE veiculos(modelo text,placa text, km_final integer)") """
 
 cursor.execute("INSERT INTO veiculos VALUES('Fiorino', 'CIY4H17', 35000)")
 cursor.execute("INSERT INTO veiculos VALUES('Fiorino', 'CIY5H18', 45000)")
 cursor.execute("INSERT INTO veiculos VALUES('Fiorino', 'BDY4H17', 70000)")
 cursor.execute("INSERT INTO veiculos VALUES('Fiorino', 'LDP4H24', 10000)")
 
-banco.commit
+banco.commit()
+
+""" cursor.execute("SELECT * FROM veiculos")
+print(cursor.fetchall()) """
